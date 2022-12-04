dc122c1764a058eab5863d63493b12f4187bb1f3
UndergroundProgrammer <88035195+UndergroundProgrammer@users.noreply.github.com>
2022-12-04 18:26:36 +0500

    Revert "reader pages added"

--- client/src/App.js
+++ client/src/App.js
@@ -1,144 +1,33 @@
-import React from "react";
-import "./App.css";
-import Dashboard from "./components/reader/Dashboard";
-import Navbar from "./components/Navbar";
-import SignIn from "./components/SignIn";
+import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SignUp from "./components/SignUp";
-import Footer from "./components/Footer";
-import Cart from "./components/Cart";
-import Faqs from "./components/Faqs";
-import Privacy from "./components/Privacy";
-import AboutUs from "./components/AboutUs";
-import MedicineList from "./components/reader/MedicineList";
-import StakeholderMapper from "./components/reader/StakeholderMapper";
-import readerServices from "./components/Services/ReaderServices";
-import authServices from "./components/Services/AuthServices";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import DoctorAppointmentMapper from "./components/poet/DoctorAppointmentMapper";
-import RespondantRequestMapper from "./components/respondant/RespondantRequestMapper";
-import AppointmentDetails from "./components/poet/AppointmentDetails";
-import RequestDetails from "./components/respondant/RequestDetails";
-import RespondantAccepted from "./components/respondant/RespondantAccepted";
-import Payment from "./components/payment";
-import Success from "./components/Success";
-import Cancel from "./components/Cancel";
-import Layout from "./components/pharmacist/dashboardComponents/home/Layout";
-import UpdateProduct from "./components/pharmacist/dashboardComponents/product/UpdateProduct";
-import DashboardComponent from "./components/pharmacist/dashboardComponents/home/dashboard";
-import AddProduct from "./components/pharmacist/dashboardComponents/product/AddProduct";
-import ProductComponent from "./components/pharmacist/dashboardComponents/product/productComponent";
-import PatientDescription from "./components/poet/PatientDescription";
-import PatientsDetail from "./components/poet/PatientsDetail";
-import MedinceDetail from "./components/reader/MedinceDetail";
-import DoctorChat from "./components/poet/Chat";
-import PatientChat from "./components/reader/PatientChat";
+import FrontPage from '../../poet-app/src/components/Pages/FrontPage'
+import LoginPage from './components/Pages/LoginPage';
+import SignupPage from './components/Pages/SignupPage';
+import ContactPage from './components/Pages/ContactPage';
+import SellerHomePage from './components/Pages/SellerPages/SellerHomePage';
+import SellerAddComponentPage from './components/Pages/SellerPages/SellerAddComponentPage';
+import SellerDeleteComponentPage from './components/Pages/SellerPages/SellerDeleteComponentPage';
+import SellerSubscriberPage from './components/Pages/SellerPages/SellerSubscriberPage';
+import SellerRequestPages from './components/Pages/SellerPages/SellerRequestPages';
+
 
 function App() {
-  const [userType, setUserType] = React.useState("");
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-  React.useEffect(() => {
-    if (authServices.getLoggedInUser() != null) {
-      var type = authServices.getLoggedInUser().userType;
-      setUserType(type);
-    } else {
-      setUserType("patient");
-    }
-
-    setIsLoggedIn(authServices.isLoggedIn());
-  }, []);
-
   return (
-    <Router>
-      {userType == "Pharmacist" && isLoggedIn ? (
-        <Layout>
-          <Routes>
-            <Route
-              path="/dashboard/products/update/:id"
-              element={<UpdateProduct />}
-            />
-            <Route path="/" element={<DashboardComponent />} />
-            <Route
-              exact
-              path="/dashboard/products"
-              element={<ProductComponent />}
-            />
-            <Route
-              path="/dashboard/products/addProduct"
-              element={<AddProduct />}
-            />
-          </Routes>
-        </Layout>
-      ) : (
-        <div className="container-fluid px-0">
-          <Navbar></Navbar>
-          <ToastContainer
-            autoClose={1000}
-            pauseOnHover={false}
-            pauseOnFocusLoss={false}
-          />
-          <Routes>
-            <Route path="/doctors/chat" element={<DoctorChat />} />
-            <Route
-              path="/doctors"
-              element={
-                <StakeholderMapper
-                  method={readerServices.getDoctors}
-                  dataType={"Doctors"}
-                />
-              }
-            />
-            <Route
-              path="/respondants"
-              element={
-                <StakeholderMapper
-                  method={readerServices.getRespondants}
-                  dataType={"Respondants"}
-                />
-              }
-            />
-            <Route path="/medicines" element={<MedicineList />} />
-            <Route path="/aboutus" element={<AboutUs />} />
-            <Route path="/faqs" element={<Faqs />} />
-            <Route path="/privacyPolicy" element={<Privacy />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/payment" element={<Payment />} />
-            <Route path="/cart/payment" element={<Cart />} />
-            <Route path="/payment/success" element={<Success />} />
-            <Route path="/payment/cancel" element={<Cancel />} />
-            <Route
-              path="/doctorAppointments/addPatientDescription"
-              element={<PatientDescription />}
-            />
 
-            <Route
-              path="/doctorAppointments"
-              element={<DoctorAppointmentMapper />}
-            />
-            <Route
-              path="/respondantRequests"
-              element={<RespondantRequestMapper />}
-            />
-            <Route path="/cart" element={<Cart />} />
-            <Route
-              path="/appointmentDetails"
-              element={<AppointmentDetails />}
-            />
-            <Route path="/medicineDetail" element={<MedinceDetail />} />
-            <Route path="/patient/chat" element={<PatientChat />} />
-            <Route path="/patientsDetail" element={<PatientsDetail />} />
-            <Route path="/requestDetails" element={<RequestDetails />} />
-            <Route path="/acceptedPatients" element={<RespondantAccepted />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/login" element={<SignIn />} />
-            <Route path="/" element={<Dashboard />} />
-          </Routes>
-          <Footer />
-        </div>
-      )}
+    <Router>
+      <Routes>
+        <Route path="/" element={<FrontPage></FrontPage>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element = {<SignupPage></SignupPage>}/>
+        <Route path="/contactus" element = {<ContactPage/>}/>
+        <Route path="/app/seller/home" element={<SellerHomePage/>}/>
+         <Route path="/app/seller/add" element={<SellerAddComponentPage/>}/>
+         <Route path="/app/seller/delete" element={<SellerDeleteComponentPage/>}/>
+         <Route path="/app/seller/requests" element={<SellerRequestPages/>}/>
+         <Route path="/app/seller/subscribers" element={<SellerSubscriberPage/>}/>
+      </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/sockets.js
+++
@@ -1,5 +0,0 @@
-import io from "socket.io-client";
-
-const socket = io.connect("https://ar-medicare-backend.herokuapp.com/");
-
-export default socket;
